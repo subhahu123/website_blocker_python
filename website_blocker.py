1d4642ccb372daf568e130ac53a60da6b72773e7
subhahu123 <34541684+subhahu123@users.noreply.github.com>
2019-06-08 09:37:33 +0530

    implementing the first part of blocker

--- website_blocker.py
+++ website_blocker.py
@@ -10,4 +10,15 @@ website_list=["www.facebook.com", "facebook.com"]
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
         print("working hours....")
+        with open(host_temp, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+    else:
+        print("fun hours....")
     time.sleep(5)
+
+    
