da5f767bee7385c47a26c3b2d091c58ff9c2e4a6
subhahu123 <34541684+subhahu123@users.noreply.github.com>
2019-06-08 08:56:54 +0530

    setting up the infinte loop

--- website_blocker.py
+++ website_blocker.py
@@ -1,5 +1,13 @@
+import time
+from datetime import datetime as dt
+
+
 hosts_path=r"C:\Windows\System32\drivers\etc\hosts"
 redirect='127.0.0.1'
 
 website_list=["www.facebook.com", "facebook.com"]
 
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+        print("working hours....")
+    time.sleep(5)
